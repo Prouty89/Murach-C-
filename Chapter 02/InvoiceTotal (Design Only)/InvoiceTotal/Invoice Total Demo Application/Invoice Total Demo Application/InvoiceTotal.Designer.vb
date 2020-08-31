bd14779583b240b001368f9a834ccc691feeb9f7
Blake <bpro0821@gmail.com>
2020-08-31 08:20:32 -0500

    add chapters

---
+++ Chapter 02/InvoiceTotal (Design Only)/InvoiceTotal/Invoice Total Demo Application/Invoice Total Demo Application/InvoiceTotal.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InvoiceTotal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Control1 = New System.Windows.Forms.Control()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSubtotal = New System.Windows.Forms.TextBox()
+        Me.txtDiscountPercent = New System.Windows.Forms.TextBox()
+        Me.txtDiscountAmount = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Control1
+        '
+        Me.Control1.Location = New System.Drawing.Point(333, 181)
+        Me.Control1.Name = "Control1"
+        Me.Control1.Size = New System.Drawing.Size(75, 23)
+        Me.Control1.TabIndex = 0
+        Me.Control1.Text = "Control1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Subtotal:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Discount Percent:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Discount Amount:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Total:"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'txtSubtotal
+        '
+        Me.txtSubtotal.Location = New System.Drawing.Point(158, 33)
+        Me.txtSubtotal.Name = "txtSubtotal"
+        Me.txtSubtotal.Size = New System.Drawing.Size(112, 20)
+        Me.txtSubtotal.TabIndex = 1
+        '
+        'txtDiscountPercent
+        '
+        Me.txtDiscountPercent.Location = New System.Drawing.Point(158, 111)
+        Me.txtDiscountPercent.Name = "txtDiscountPercent"
+        Me.txtDiscountPercent.ReadOnly = True
+        Me.txtDiscountPercent.Size = New System.Drawing.Size(112, 20)
+        Me.txtDiscountPercent.TabIndex = 6
+        Me.txtDiscountPercent.TabStop = False
+        '
+        'txtDiscountAmount
+        '
+        Me.txtDiscountAmount.Location = New System.Drawing.Point(158, 85)
+        Me.txtDiscountAmount.Name = "txtDiscountAmount"
+        Me.txtDiscountAmount.ReadOnly = True
+        Me.txtDiscountAmount.Size = New System.Drawing.Size(112, 20)
+        Me.txtDiscountAmount.TabIndex = 7
+        Me.txtDiscountAmount.TabStop = False
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(158, 59)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(112, 20)
+        Me.txtTotal.TabIndex = 8
+        Me.txtTotal.TabStop = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCalculate.Location = New System.Drawing.Point(96, 144)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(195, 144)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'InvoiceTotal
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(350, 201)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtDiscountAmount)
+        Me.Controls.Add(Me.txtDiscountPercent)
+        Me.Controls.Add(Me.txtSubtotal)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Control1)
+        Me.Name = "InvoiceTotal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Invoice Total"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Control1 As Control
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSubtotal As TextBox
+    Friend WithEvents txtDiscountPercent As TextBox
+    Friend WithEvents txtDiscountAmount As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Chapter 02/InvoiceTotal (Design Only)/InvoiceTotal/Invoice Total Demo Application/Invoice Total Demo Application/InvoiceTotal.vb
@@ -0,0 +1,9 @@
+﻿Public Class InvoiceTotal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
